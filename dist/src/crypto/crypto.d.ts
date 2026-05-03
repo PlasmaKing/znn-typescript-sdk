865bc8866d798893ed364f6db4f14162efe0b396
ZŌE <zoe@zenon.red>
2026-05-03 01:32:25 -0400

    workaround: prebuilt dist for probe

---
+++ dist/src/crypto/crypto.d.ts
@@ -0,0 +1,11 @@
+import { Buffer } from "buffer";
+import { BytesLike } from "../utilities/bytes.js";
+export declare class Crypto {
+    static getPublicKey(privateKey: Buffer): Buffer;
+    static deriveKey(path: string, seed: string): Buffer;
+    static sign(message: Buffer, privateKey: Buffer): Buffer;
+    static digest(data: Buffer): Uint8Array<ArrayBufferLike>;
+    static keccak256(data: BytesLike): string;
+    static randomBytes(length?: number): Buffer;
+}
+//# sourceMappingURL=crypto.d.ts.map
